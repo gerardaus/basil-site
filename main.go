b58fff1bf1636958f1eb72e03b051922ca9afbeb
Gerard Paulke <Gerard.Paulke@quantium.com.au>
2017-10-20 15:11:11 -0700

    first pass

---
+++ main.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+func EnvHandler(rw http.ResponseWriter, req *http.Request) {
+	environment := make(map[string]string)
+	for _, item := range os.Environ() {
+		splits := strings.Split(item, "=")
+		key := splits[0]
+		val := strings.Join(splits[1:], "=")
+		environment[key] = val
+	}
+
+	envJSON := HandleError(json.MarshalIndent(environment, "", "  ")).([]byte)
+	rw.Write(envJSON)
+}
+
+func HandleError(result interface{}, err error) (r interface{}) {
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.Path("/env").Methods("GET").HandlerFunc(EnvHandler)
+
+	n := negroni.Classic()
+	n.UseHandler(r)
+	n.Run(":3000")
+}
